2de2b7a5034bf2257eda778f24d7175a6c94c8f8
Hoang M. Le <hle@cs.uni-bremen.de>
2016-04-14 07:44:39 -0400

    update PROFILING flags

--- cmake/build_type.cmake
+++ cmake/build_type.cmake
@@ -1,23 +1,23 @@
 #
 # Add the build_type "Profiling", that
 # * enables gcov profiling
-# * enables -Wall (all warnings)
+# * enables -Wall -Wextra -pedantic
 # * disables optimizations
 # for GCC or compatible compilers
 #
 
-SET( CMAKE_CXX_FLAGS_PROFILING "-O0 -g -Wall -fprofile-arcs -ftest-coverage" CACHE STRING
+SET( CMAKE_CXX_FLAGS_PROFILING "-O0 -g -Wall -Wextra -pedantic --coverage" CACHE STRING
   "Flags used by the C++ compiler during profiling builds."
   FORCE )
-SET( CMAKE_C_FLAGS_PROFILING "-O0 -g -Wall -fprofile-arcs -ftest-coverage" CACHE STRING
+SET( CMAKE_C_FLAGS_PROFILING "${CMAKE_CXX_FLAGS_PROFILING}" CACHE STRING
   "Flags used by the C compiler during profiling builds."
   FORCE )
 SET( CMAKE_EXE_LINKER_FLAGS_PROFILING
-  "-fprofile-arcs -ftest-coverage" CACHE STRING
+  "--coverage" CACHE STRING
   "Flags used for linking binaries during profiling builds."
   FORCE )
 SET( CMAKE_SHARED_LINKER_FLAGS_PROFILING
-  "-fprofile-arcs -ftest-coverage" CACHE STRING
+  "--coverage" CACHE STRING
   "Flags used by the shared libraries linker during profiling builds."
   FORCE )
 MARK_AS_ADVANCED(
