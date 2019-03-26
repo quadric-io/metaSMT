76efbf0c75fcbd79e3da656f0fb51700353893d2
Hoang M. Le <hle@cs.uni-bremen.de>
2019-03-26 12:12:22 +0100

    detect Z3 API changes (since v4.7) and set compile flags accordingly

--- src/metaSMT/backend/Z3_Backend.hpp
+++ src/metaSMT/backend/Z3_Backend.hpp
@@ -147,7 +147,11 @@ namespace metaSMT {
         }
 
         assert(r.is_bv());
+#ifdef metaSMT_Z3_ULL_API
         unsigned long long val = 0;
+#else
+        uint64_t val = 0;
+#endif
         if (Z3_get_numeral_uint64(ctx_, r, &val) && val <= std::numeric_limits<uint64_t>::max())
           return result_wrapper(val, r.get_sort().bv_size());
 
